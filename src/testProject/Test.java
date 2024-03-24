81cc516fbc11a3e65add2a0690492475d7e563e9
GyungAe Kim <ko2931@naver.com>
2024-03-24 13:02:56 +0900

    clear

--- src/testProject/Test.java
+++ src/testProject/Test.java
@@ -3,18 +3,7 @@ package testProject;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("KGA first(master)");
-		System.out.println("KGA feature-A");
-		System.out.println("KGA feature-A2");
-		System.out.println("KGA feature-AA");
-		System.out.println("KGA feature-AAA");
-		System.out.println("KGA feature-B");
-		System.out.println("KGA feature-BB");
-		System.out.println("KGA feature-BB2");
-		System.out.println("KGA feature-BB-conflict-solved");
-		System.out.println("KKA progress-A");
-		System.out.println("KKA progress-AA");
-		System.out.println("KKA progress-B");
+		
 	}
 
 }
